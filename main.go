618f85d1245d97457f57add58c91872883a8120a
Philip Porter <philaporter@gmail.com>
2018-10-06 11:15:24 -0700

    added put, post, and delete routes

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 	"encoding/json"
+	"log"
 )
 
 type Superhero struct {
@@ -16,14 +16,27 @@ type Superhero struct {
 var superheroes []Superhero
 
 func main() {
+	createRecords()
+	createRoutes()
+}
+
+func createRecords() {
 	superheroes = append(superheroes, Superhero{ID: "-1", Name: "MissingNo.", Superpower: "Item duplication"})
 	superheroes = append(superheroes, Superhero{ID: "1", Name: "Dodger", Superpower: "Dodger of all things unwanted"})
+	superheroes = append(superheroes, Superhero{ID: "2", Name: "Freezer", Superpower: "Makes things really, really cold"})
+	superheroes = append(superheroes, Superhero{ID: "3", Name: "Mr. Owl", Superpower: "Knows how many licks it takes"})
+}
 
+func createRoutes() {
 	router := mux.NewRouter()
 	router.HandleFunc("/superhero", GetSuperheroes).Methods("GET")
 	router.HandleFunc("/superhero/{id}", GetSuperhero).Methods("GET")
+	router.HandleFunc("/superhero/{id}", PutSuperhero).Methods("PUT")
+	router.HandleFunc("/superhero/{id}", PostSuperhero).Methods("POST")
+	router.HandleFunc("/superhero/{id}", DeleteSuperhero).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
 func GetSuperhero(writer http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
 	for _, hero := range superheroes {
@@ -34,6 +47,36 @@ func GetSuperhero(writer http.ResponseWriter, request *http.Request) {
 	}
 	json.NewEncoder(writer).Encode(superheroes[0])
 }
+
 func GetSuperheroes(writer http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(writer).Encode(superheroes)
+}
+
+func PutSuperhero(writer http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	var hero Superhero
+	_ = json.NewDecoder(request.Body).Decode(&hero)
+	hero.ID = params["id"]
+	superheroes = append(superheroes, hero)
+	json.NewEncoder(writer).Encode(hero)
+}
+
+func PostSuperhero(writer http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	var hero Superhero
+	_ = json.NewDecoder(request.Body).Decode(&hero)
+	hero.ID = params["id"]
+	superheroes = append(superheroes, hero)
+	json.NewEncoder(writer).Encode(hero)
+}
+
+func DeleteSuperhero(writer http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	for index, hero := range superheroes {
+		if hero.ID == params["id"] {
+			superheroes = append(superheroes[:index], superheroes[index+1:]...)
+			json.NewEncoder(writer).Encode(hero)
+			break
+		}
+	}
 }
